f30375023261db4e30aea4547e847681cdc0c24f
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2022-12-13 10:57:11 +0100

    custom_functions.ts bijwerken

--- custom_functions.ts
+++ custom_functions.ts
@@ -1,17 +1,5 @@
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
-    //% block="unlockWorld code:$code"
-    //% block.loc.nl="ontgrendelWereld $code"
-    export function unlockWorld(code: string) {
-        if (code === "code123") {
-            player.execute("execute @a[tag=!unlocked] ~ ~ ~ dialogue change @e[tag=claire1] claire1")
-            player.execute("execute @a[tag=!unlocked] ~ ~ ~ say Unlocked!")
-            player.execute("execute @a[tag=!unlocked] ~ ~ ~ tag @a add unlocked")
-        } else {
-            player.execute("tag @a remove unlocked");
-        }
-    }
-
     //% block="startingPosition position:$position"
     //% block.loc.nl="zetKlaar positie:$position"
     export function startingPosition(position:Position) {
