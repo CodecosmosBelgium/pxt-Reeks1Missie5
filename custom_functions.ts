a4ea3667f61302d5e54f222722870c5011f9960c
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2023-01-17 13:02:14 +0100

    Update custom_functions.ts

--- custom_functions.ts
+++ custom_functions.ts
@@ -1,14 +1,12 @@
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
-    //% block="startingPosition position:$position"
-    //% block.loc.nl="zetKlaar positie:$position"
-    export function startingPosition(position:Position) {
+    //% block="zet agent klaar op $position"
+    export function startingPosition(position:Position=world(0,0,0)) {
         player.execute("function levels/startingPositions");
         agent.teleport(position, SOUTH);
     }
 
-    //% block="position below agent"
-    //% block.loc.nl_NL="positie onder agent"
+    //% block="positie onder agent"
     export function positionBelowAgent() {
         return world(agent.getPosition().getValue(Axis.X), agent.getPosition().getValue(Axis.Y) - 1, agent.getPosition().getValue(Axis.Z))
     }
